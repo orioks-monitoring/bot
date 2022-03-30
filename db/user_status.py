7a6148fa4f14c7881294aee41a76ce03b091a653
Кирилл <kkulikov14@list.ru>
2022-03-31 00:03:47 +0300

    fix: bug with admins statistics in marks count

--- db/user_status.py
+++ db/user_status.py
@@ -89,38 +89,3 @@ def update_inc_user_orioks_attempts(user_telegram_id: int) -> None:
     })
     db.commit()
     db.close()
-
-
-def select_count_user_status_statistics() -> dict:
-    db = sqlite3.connect(config.PATH_TO_DB)
-    sql = db.cursor()
-    with open(os.path.join(config.PATH_TO_SQL_FOLDER, 'select_count_user_status_statistics.sql'), 'r') as sql_file:
-        sql_script = sql_file.read()
-    users_agreement_accepted = sql.execute(
-        sql_script.format(row_name='is_user_agreement_accepted'), {
-            'value': True,
-        }
-    ).fetchone()
-    users_agreement_discarded = sql.execute(
-        sql_script.format(row_name='is_user_agreement_accepted'), {
-            'value': False,
-        }
-    ).fetchone()
-
-    users_orioks_authentication = sql.execute(
-        sql_script.format(row_name='is_user_orioks_authenticated'), {
-            'value': True,
-        }
-    ).fetchone()
-    users_orioks_no_authentication = sql.execute(
-        sql_script.format(row_name='is_user_orioks_authenticated'), {
-            'value': False,
-        }
-    ).fetchone()
-    db.close()
-    all_users = users_agreement_discarded[0] + users_agreement_accepted[0]
-    all_orioks_users = users_orioks_no_authentication[0] + users_orioks_authentication[0]
-    return {
-        'Приняли пользовательское соглашение': f'{users_agreement_accepted[0]} / {all_users}',
-        'Выполнили вход в ОРИОКС': f'{users_orioks_authentication[0]} / {all_orioks_users}',
-    }
--- handlers/admins.py
+++ handlers/admins.py
@@ -3,26 +3,34 @@ import aiogram.utils.markdown as md
 
 import handlers.notify_settings
 import db.admins_statistics
-import db.user_status
 import db.notify_settings
 
 
 async def admin_get_statistics(message: types.Message):
     msg = ''
-    for key, value in db.user_status.select_count_user_status_statistics().items():
+    for key, value in db.admins_statistics.select_count_user_status_statistics().items():
         msg += md.text(
             md.text(key),
             md.text(value),
             sep=': ',
         ) + '\n'
     msg += '\n'
+
     for key, value in db.notify_settings.select_count_notify_settings_statistics().items():
-        msg += md.text(
-            md.text(handlers.notify_settings.notify_settings_names_to_vars[key]),
-            md.text(value),
-            sep=': ',
-        ) + '\n'
+        if key == 'marks':
+            msg += md.text(
+                md.text(handlers.notify_settings.notify_settings_names_to_vars[key]),
+                md.text(db.admins_statistics.select_count_notify_settings_marks()),
+                sep=': ',
+            ) + '\n'
+        else:
+            msg += md.text(
+                md.text(handlers.notify_settings.notify_settings_names_to_vars[key]),
+                md.text(value),
+                sep=': ',
+            ) + '\n'
     msg += '\n'
+
     for key, value in db.admins_statistics.select_all_from_admins_statistics().items():
         msg += md.text(
             md.text(key),
