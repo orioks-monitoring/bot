ad2be1e443307018762d6124624dbc2c3206bd4e
Кирилл <kkulikov14@list.ru>
2022-03-31 18:24:52 +0300

    orioks dont die please

--- utils/make_request.py
+++ utils/make_request.py
@@ -1,8 +1,11 @@
+import time
+
 import db.admins_statistics
 import aiohttp
 
 
 async def get_request(url: str, session: aiohttp.ClientSession) -> str:
+    time.sleep(1)  # orioks, dont die please
     async with session.get(str(url)) as resp:
         raw_html = await resp.text()
     db.admins_statistics.update_inc_admins_statistics_row_name(  # TODO: sum of requests and inc for one use db
