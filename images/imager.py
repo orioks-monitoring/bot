b497ed97f578159669768b073e7a288569a1d675
Кирилл <kkulikov14@list.ru>
2022-05-20 16:31:23 +0300

    fix: mark_change_text disp without split #14

--- images/imager.py
+++ images/imager.py
@@ -64,7 +64,8 @@ class Imager:
     def _calculate_count_of_lines_by_width_line(self, text: str) -> int:
         return len(textwrap.wrap(text, width=self._width_line))
 
-    def _calculate_container(self, title_text, side_text, mark_change_text=None, need_qr=False):
+    def _calculate_container(self, title_text, side_text, mark_change_text=None, need_qr=False,
+                             need_split_mark_change_text=False):
         container_height = 0
 
         for line in textwrap.wrap(title_text, width=self._width_line):
@@ -78,7 +79,10 @@ class Imager:
             container_height += self._font_upper.getsize(line)[1]
 
         if mark_change_text:
-            for line in textwrap.wrap(mark_change_text, width=self._width_line):
+            for line in textwrap.wrap(
+                    mark_change_text,
+                    width=self._width_line if need_split_mark_change_text else len(mark_change_text)
+            ):
                 container_height += self._font_upper.getsize(line)[1]
 
             container_height += self._font_upper.getsize(mark_change_text)[1] / 2
@@ -110,9 +114,13 @@ class Imager:
             text,
             font,
             fill,
-            offset
+            offset,
+            need_split=True
     ):
-        for line in textwrap.wrap(text, width=self._width_line):
+        width_to_split = self._width_line
+        if not need_split:
+            width_to_split = len(text)
+        for line in textwrap.wrap(text, width=width_to_split):
             self.draw_text.text(
                 ((self.image_weight - self.draw_text.textsize(line, font=font)[0]) / 2,
                  (self.image_height - self.container_height) / 2 + offset),
@@ -146,12 +154,12 @@ class Imager:
         offset = 0
         offset = self._draw_text(title_text, self._font_upper, self._fill_upper, offset)
         offset += self._font_upper.getsize(title_text)[1] / 4
-        offset = self._draw_text(mark_change_text, self._font_upper, self._fill_upper, offset)
+        offset = self._draw_text(mark_change_text, self._font_upper, self._fill_upper, offset, need_split=False)
         offset += self._font_upper.getsize(title_text)[1] / 4
         self._draw_text(side_text, self._font_downer, self._fill_downer, offset)
 
-    def _calculate_font_size_and_text_width(self, title_text, side_text):
-        self._calculate_container(title_text, side_text, need_qr=True)
+    def _calculate_font_size_and_text_width(self, title_text, side_text, need_qr: bool = False, mark_change_text=None):
+        self._calculate_container(title_text, side_text, need_qr=need_qr, mark_change_text=mark_change_text)
         if self.container_height <= 670:
             return True
         if self.container_height > 670 and self.container_width < 1200:
@@ -160,7 +168,8 @@ class Imager:
         self._font_downer_size -= 1
         self._font_upper = ImageFont.truetype(self._font_path, size=self._font_upper_size)
         self._font_downer = ImageFont.truetype(self._font_path, size=self._font_downer_size)
-        self._calculate_font_size_and_text_width(title_text, side_text)
+        self._calculate_font_size_and_text_width(title_text, side_text, need_qr=need_qr,
+                                                 mark_change_text=mark_change_text)
 
     def get_image_marks(
             self,
@@ -173,8 +182,7 @@ class Imager:
         current_grade = 0 if current_grade == 'н' else current_grade
         max_grade = 1 if max_grade == 0 else max_grade
         self._get_image_by_grade(current_grade, max_grade)
-        self._calculate_font_size_and_text_width(title_text, side_text)
-        self._calculate_container(title_text, side_text, mark_change_text=mark_change_text)
+        self._calculate_font_size_and_text_width(title_text, side_text, mark_change_text=mark_change_text)
         self._draw_text_marks(title_text, mark_change_text, side_text)
         path_to_result_image = pathlib.Path(os.path.join(config.BASEDIR, f'temp_{secrets.token_hex(15)}.png'))
         self.image.save(path_to_result_image)
@@ -189,7 +197,7 @@ class Imager:
         self._get_news_image()
         if title_text == '':
             return self.image
-        self._calculate_font_size_and_text_width(title_text, side_text)
+        self._calculate_font_size_and_text_width(title_text, side_text, need_qr=True)
         self._draw_text_news(title_text, side_text, url)
         path_to_result_image = pathlib.Path(os.path.join(config.BASEDIR, f'temp_{secrets.token_hex(15)}.png'))
         self.image.save(path_to_result_image)
