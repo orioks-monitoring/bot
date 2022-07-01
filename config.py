2d5eae6dca918e5826ebbebbbff356e1eac28196
Кирилл <kkulikov14@list.ru>
2022-07-01 21:39:40 +0300

    fix(checking): remove br requests encoding

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ ORIOKS_LOGIN_QUEUE_SEMAPHORE_VALUE = 1
 
 ORIOKS_REQUESTS_HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'ru-RU,ru;q=0.9',
     'User-Agent': 'orioks_monitoring/1.0 (Linux; aiohttp)'
 }
